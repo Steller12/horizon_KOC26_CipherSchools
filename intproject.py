788c7f1bbd0c0b34f869058ea247895635e31c6c
Pushp Gupta <pugpta2601@gmail.com>
2022-11-22 20:32:58 +0530

    edit

--- intproject.py
+++ intproject.py
@@ -1,10 +1,13 @@
+#taking input from user
 date_of_birth=int(input("date of birth:"))
 month_of_birth=int(input("month of birth:"))
 year_of_birth=int(input("year of birth:"))
 current_day=int(input("current day:"))
 current_month=int(input("current month:"))
 current_year=int(input("current year:"))
+#years between year or birth and current year
 year=current_year-year_of_birth
+#getting month and day in positive
 if current_month>month_of_birth:
     month=current_month-month_of_birth
 else:
@@ -13,16 +16,9 @@ if current_day>date_of_birth:
     day=current_day-date_of_birth
 else:
     day=date_of_birth-current_day
-if year_of_birth<2020:
-    day=day+1
-    if year<2016:
+    month=month-1
+#logic to add days for leap years one by one
+for i in range(year_of_birth,current_year+1):
+    if i % 400 == 0  or (i % 4 == 0 and i % 100 != 0):
         day=day+1
-        if year<2012:
-            day=day+1
-            if year<2008:
-                day=day+1
-                if year<2004:
-                    day=day+1
-                    if year<2000:
-                        day=day+1
 print("your age is",day,"days",month,"months",year,"years")
